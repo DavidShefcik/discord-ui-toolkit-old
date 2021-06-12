26c95e616f11a9a4c61fcbc1759ec2dfab7f0107
David Shefcik <davidshefcik@gmail.com>
2021-06-12 13:51:23 -0500

    Configuration changes.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'discord-dark',
+    values: [
+      {
+        name: 'discord-dark',
+        value: '#36393f'
+      },
+      {
+        name: 'discord-light',
+        value: '#ffffff'
+      }
+    ]
+  }
 }
