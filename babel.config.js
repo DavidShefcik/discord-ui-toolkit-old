3f47811c9fb1e74926daed89751a61224fe9a4a3
David Shefcik <davidshefcik@gmail.com>
2021-10-10 14:11:47 -0500

    Rollup configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
   plugins: ['emotion', '@babel/plugin-transform-runtime'],
+  env: {
+    production: {
+      plugins: [
+        'babel-plugin-jsx-remove-data-test-id',
+        {
+          attributes: ['data-testid'],
+        },
+      ],
+    },
+  },
 };
---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import ts from 'rollup-plugin-ts';
+import postcss from 'rollup-plugin-postcss';
+import image from '@rollup/plugin-image';
+import alias from '@rollup/plugin-alias';
+import url from '@rollup/plugin-url';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import copy from 'rollup-plugin-copy';
+import json from '@rollup/plugin-json';
+
+import path from 'path';
+
+/* eslint-disable-next-line */
+const packageJson = require('./package.json');
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    image(),
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescriptPaths(),
+    url({
+      include: ['**/*.svg', '**/*.png', '**/*.jp(e)?g', '**/*.gif', '**/*.webp', '**/*.webm'],
+    }),
+    copy({
+      targets: [
+        {
+          src: './src/assets/',
+          dest: 'build',
+        },
+      ],
+    }),
+    ts({
+      tsconfig: 'tsconfig.build.json',
+    }),
+    json(),
+    alias({
+      entries: [
+        {
+          find: '@assets',
+          replacement: path.resolve(__dirname, 'src/assets'),
+        },
+      ],
+    }),
+    postcss(),
+  ],
+};
