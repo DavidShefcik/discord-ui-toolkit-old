5cd4ec615072a3a3356e3829f28301c2385029d2
DavidShefcik <davidshefcik@gmail.com>
2021-11-06 19:50:39 -0500

    TextInput label and error text

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -6,7 +6,9 @@ interface TextInputProps {
   onChange(value: string): void;
   onEnterPress?(value: string): void;
   htmlType?: 'text' | 'password' | 'email' | 'phone' | 'email';
+  label?: string;
   error?: boolean;
+  errorText?: string;
   placeholder?: string;
   maxLength?: number;
   disabled?: boolean;
@@ -20,6 +22,33 @@ interface TextInputProps {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  label: {
+    marginBottom: '8px',
+    fontSize: '12px',
+    lineHeight: '16px',
+  },
+  labelText: {
+    color: 'var(--header-secondary)',
+    fontFamily: 'discord-bold',
+    textTransform: 'uppercase',
+  },
+  redLabel: {
+    color: 'var(--red)',
+  },
+  labelErrorSeparator: {
+    padding: '0 4px',
+  },
+  labelErrorText: {
+    color: 'var(--red)',
+    fontFamily: 'discord-normal',
+    fontStyle: 'italic',
+  },
   inputBase: {
     backgroundColor: 'var(--text-input-background)',
     color: 'var(--text-normal)',
@@ -29,7 +58,6 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: '100%',
-    height: '100%',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'flex-start',
@@ -48,8 +76,10 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: '0',
   },
-  disabled: {
+  disabledCursor: {
     cursor: 'not-allowed',
+  },
+  disabledOpacity: {
     opacity: 0.5,
   },
   borderDark: {
@@ -73,7 +103,9 @@ export default function TextInput({
   onChange,
   onEnterPress,
   htmlType = 'text',
+  label = '',
   error = false,
+  errorText,
   placeholder,
   maxLength,
   disabled = false,
@@ -96,41 +128,57 @@ export default function TextInput({
     }
   };
 
+  let labelVisible = label || errorText;
+  let labelDashVisible = label && errorText;
+
   return (
-    <div style={{ display: 'inline-block', width, height }}>
-      <div
-        className={css([
-          styles.inputBase,
-          styles.inputContainer,
-          disabled && styles.disabled,
-          error
-            ? styles.borderRed
-            : [
-                styles.borderDark,
-                !disabled && !focused && styles.borderDarkPsuedos,
-                focused && styles.borderDarkFocused,
-              ],
-        ])}
-        style={{ fontSize }}
-      >
-        {prefix && prefix.length > 0 && <span>{prefix}</span>}
-        <input
-          className={css([styles.inputBase, styles.input])}
-          type={htmlType}
-          value={value}
-          onChange={(event) => onChange(event.target.value)}
-          // @ts-ignore
-          onKeyDown={onKeyDownEvent}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-          placeholder={placeholder}
-          maxLength={maxLength}
-          disabled={disabled}
-          autoComplete={autoComplete ? 'on' : 'off'}
-          spellCheck={spellcheck}
-          autoFocus={autoFocus}
-          style={{ fontSize }}
-        />
+    <div style={{ display: 'inline-block', width }}>
+      <div className={css(styles.container)}>
+        {labelVisible && (
+          <span className={css(styles.label)}>
+            <span className={css([styles.labelText, error && styles.redLabel])}>{label}</span>
+            {error && errorText && (
+              <span className={css(styles.labelErrorText)}>
+                {labelDashVisible && <span className={css(styles.labelErrorSeparator)}>-</span>}
+                {errorText}
+              </span>
+            )}
+          </span>
+        )}
+        <div
+          className={css([
+            styles.inputBase,
+            styles.inputContainer,
+            disabled && [styles.disabledOpacity, styles.disabledCursor],
+            error
+              ? styles.borderRed
+              : [
+                  styles.borderDark,
+                  !disabled && !focused && styles.borderDarkPsuedos,
+                  focused && styles.borderDarkFocused,
+                ],
+          ])}
+          style={{ fontSize, height }}
+        >
+          {prefix && prefix.length > 0 && <span>{prefix}</span>}
+          <input
+            className={css([styles.inputBase, styles.input, disabled && styles.disabledCursor])}
+            type={htmlType}
+            value={value}
+            onChange={(event) => onChange(event.target.value)}
+            // @ts-ignore
+            onKeyDown={onKeyDownEvent}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            placeholder={placeholder}
+            maxLength={maxLength}
+            disabled={disabled}
+            autoComplete={autoComplete ? 'on' : 'off'}
+            spellCheck={spellcheck}
+            autoFocus={autoFocus}
+            style={{ fontSize }}
+          />
+        </div>
       </div>
     </div>
   );
